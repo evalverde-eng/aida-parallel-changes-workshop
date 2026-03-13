65003a607e86e1f879f585b61b7190fdf7a7a73f
Emmanuel Valverde Ramos <evalverde@domingoalonsogroup.com>
2026-03-13 16:59:42 +0100

    test: replace infrastructure placeholders with executable specs

--- tests/Aida.ParallelChange.Api.Tests/Infrastructure/MigrationTests.cs
+++ tests/Aida.ParallelChange.Api.Tests/Infrastructure/MigrationTests.cs
@@ -1,11 +1,42 @@
+using Aida.ParallelChange.Api.Infrastructure.Persistence.Migrations;
+using FluentMigrator;
+using Shouldly;
+
 namespace Aida.ParallelChange.Api.Tests.Infrastructure;
 
 [TestFixture]
 public sealed class MigrationTests
 {
     [Test]
-    public void Placeholder_for_real_sql_server_migration_tests()
+    public void Migrations_include_initial_schema_version()
     {
-        Assert.Pass();
+        var migrationVersions = typeof(CreateCustomerContactsTable)
+            .Assembly
+            .GetTypes()
+            .Where(type => type.IsSubclassOf(typeof(Migration)))
+            .Select(type => type.GetCustomAttributes(typeof(MigrationAttribute), inherit: false)
+                .OfType<MigrationAttribute>()
+                .Single()
+                .Version)
+            .OrderBy(version => version)
+            .ToArray();
+
+        migrationVersions.ShouldContain(202604010001);
+    }
+
+    [Test]
+    public void Migration_versions_are_unique()
+    {
+        var migrationVersions = typeof(CreateCustomerContactsTable)
+            .Assembly
+            .GetTypes()
+            .Where(type => type.IsSubclassOf(typeof(Migration)))
+            .Select(type => type.GetCustomAttributes(typeof(MigrationAttribute), inherit: false)
+                .OfType<MigrationAttribute>()
+                .Single()
+                .Version)
+            .ToArray();
+
+        migrationVersions.Distinct().Count().ShouldBe(migrationVersions.Length);
     }
 }
--- tests/Aida.ParallelChange.Api.Tests/Infrastructure/SqlServerCustomerContactRepositoryTests.cs
+++ tests/Aida.ParallelChange.Api.Tests/Infrastructure/SqlServerCustomerContactRepositoryTests.cs
@@ -1,11 +1,26 @@
+using Aida.ParallelChange.Api.Infrastructure.Persistence.SqlServer;
+using Shouldly;
+
 namespace Aida.ParallelChange.Api.Tests.Infrastructure;
 
 [TestFixture]
 public sealed class SqlServerCustomerContactRepositoryTests
 {
     [Test]
-    public void Placeholder_for_real_sql_server_repository_tests()
+    public void FindById_query_reads_identity_and_email_fields()
     {
-        Assert.Pass();
+        SqlQueries.FindById.ShouldContain("customer_id");
+        SqlQueries.FindById.ShouldContain("email");
+        SqlQueries.FindById.ShouldContain("WHERE customer_id = @CustomerId");
+    }
+
+    [Test]
+    public void Upsert_query_uses_merge_with_update_and_insert_paths()
+    {
+        SqlQueries.Upsert.ShouldContain("MERGE customer_contacts");
+        SqlQueries.Upsert.ShouldContain("WHEN MATCHED");
+        SqlQueries.Upsert.ShouldContain("WHEN NOT MATCHED");
+        SqlQueries.Upsert.ShouldContain("@CustomerId");
+        SqlQueries.Upsert.ShouldContain("@Email");
     }
 }
